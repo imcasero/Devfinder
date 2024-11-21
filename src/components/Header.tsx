05582efaa25af55eb8793d7e21fff4adc2931d3b
Imcasero <diegocaserosmr@gmail.com>
2024-11-21 19:14:11 +0100

    feat: update toggle theme button styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,9 +4,11 @@ export const Header = () => {
   const { toggleTheme, theme } = useTheme();
   return (
     <header className="w-full flex justify-between align-middle">
-      <h1 className="font-bold text-xl text-primary">Devfinder</h1>
+      <h1 className="font-bold text-xl text-primary flex justify-center align-middle">
+        DevFinder
+      </h1>
       <button
-        className="text-sm text-primary flex items-center justify-center w-8 h-8 rounded-md  hover:bg-primary hover:text-white transition"
+        className="text-sm text-primary flex items-center justify-center w-8 h-8 rounded-md hover:bg-primary hover:bg-opacity-20  transition"
         onClick={() => {
           toggleTheme();
         }}

423b47edf09995931c772bd82fe56ebc4c741b5c
Imcasero <diegocaserosmr@gmail.com>
2024-11-21 17:53:49 +0100

    feat: add icon theme toggle

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,21 @@
 import { useTheme } from "@context/themeContext";
+import { Sun, Moon } from "lucide-react";
 export const Header = () => {
   const { toggleTheme, theme } = useTheme();
   return (
     <header className="w-full flex justify-between align-middle">
       <h1 className="font-bold text-xl text-primary">Devfinder</h1>
       <button
-        className="text-base text-primary border-2 border-primary w-20 h-8 rounded-lg"
+        className="text-sm text-primary flex items-center justify-center w-8 h-8 rounded-md  hover:bg-primary hover:text-white transition"
         onClick={() => {
           toggleTheme();
         }}
       >
-        {String(theme).charAt(0).toUpperCase() + String(theme).slice(1)}
+        {theme === "dark" ? (
+          <Sun className="w-4 h-4" />
+        ) : (
+          <Moon className="w-4 h-4" />
+        )}
       </button>
     </header>
   );
